2a6a722b7e57e15e70ef6b71b10b64e1e5a0e7d2
Jacob Parry <jacob@parry.co>
2025-07-22 20:54:43 -0600

    add buttons and add functionality for print button

--- frontend/src/app/pages/resume/resume.component.ts
+++ frontend/src/app/pages/resume/resume.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ResumeService, ResumeData } from '../../services/resume.service';
 import { MainTemplateComponent } from '../../components/templates/main-template/main-template.component';
+import { NgxPrintModule } from 'ngx-print';
 
 @Component({
   selector: 'app-resume',
   standalone: true,
-  imports: [CommonModule, MainTemplateComponent],
+  imports: [CommonModule, MainTemplateComponent, NgxPrintModule],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.css'
 })
